b2a119e4e514de33267e1f661e9c9516954e5829
John <john.t.singer@gmail.com>
2023-06-13 09:39:12 +0200

    Create module to store ansi colors

---
+++ utils/ansi_colors.py
@@ -0,0 +1,7 @@
+# Colors
+R = "\033[0;31;40m"  # RED
+G = "\033[0;32;40m"  # GREEN
+Y = "\033[0;33;40m"  # Yellow
+B = "\033[0;34;40m"  # Blue
+BL = "\033[38;5;75m"  # Blue #5FAFF
+N = "\033[0m"  # Reset
